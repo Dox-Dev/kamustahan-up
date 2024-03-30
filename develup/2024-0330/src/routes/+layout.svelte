4ecffa73a51a016029af7a023cc2409d15824d92
Dox-Dev <83331542+Dox-Dev@users.noreply.github.com>
2024-03-31 00:21:24 +0800

    feat(`2024-0330`): modify website `AppShell` and `favicon`

--- develup/2024-0330/src/routes/+layout.svelte
+++ develup/2024-0330/src/routes/+layout.svelte
@@ -9,32 +9,32 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">DevelUP Kamustahan</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
+					href="https://discord.com/users/242108300966100993"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Discord
+					<i class="fa-brands fa-discord pr-2"/> morthodox
+				</a>
+				<a
+				class="btn btn-sm variant-ghost-surface"
+				href="https://github.com/Dox-Dev"
+				target="_blank"
+				rel="noreferrer"
+				>
+					<i class="fa-brands fa-github pr-2"/> Dox-Dev
 				</a>
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
+					href="https://www.facebook.com/joseellis.caluag/"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
+					<i class="fa-brands fa-facebook pr-2"/> Jose Ellis Miguel Caluag
 				</a>
 			</svelte:fragment>
 		</AppBar>
